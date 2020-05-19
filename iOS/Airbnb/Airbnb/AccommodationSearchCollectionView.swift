99dc0808748da760e34b369136951b429ddd352c
idevjinie <idevjinie@gmail.com>
2020-05-19 15:51:45 +0900

    [#1] feat :rocket: 컬렉션뷰의 셀 구성

---
+++ iOS/Airbnb/Airbnb/AccommodationSearchCollectionView.swift
@@ -0,0 +1,21 @@
+//
+//  AccommodationSearchCollectionView.swift
+//  Airbnb
+//
+//  Created by jinie on 2020/05/19.
+//  Copyright © 2020 jinie. All rights reserved.
+//
+
+import UIKit
+
+class AccommodationSearchCollectionView: UICollectionView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ iOS/Airbnb/Airbnb/AccommodationSearchCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  AccommodationSearchCollectionViewCell.swift
+//  Airbnb
+//
+//  Created by jinie on 2020/05/19.
+//  Copyright © 2020 jinie. All rights reserved.
+//
+
+import UIKit
+
+class AccommodationSearchCollectionViewCell: UICollectionViewCell {
+    
+}
