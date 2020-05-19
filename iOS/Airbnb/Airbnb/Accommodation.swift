832d8ab5feecbd06d1b6a5f9c137923945a7c618
delma <sdy2856@gmail.com>
2020-05-19 17:40:53 +0900

    [#7] feat :rocket: Accomodation 모델 생성

---
+++ iOS/Airbnb/Airbnb/Accommodation.swift
@@ -0,0 +1,20 @@
+//
+//  Accommodation.swift
+//  Airbnb
+//
+//  Created by delma on 2020/05/19.
+//  Copyright © 2020 jinie. All rights reserved.
+//
+
+import Foundation
+
+struct Accommodation: Codable {
+    var name: String
+    var imageURLs: [String]
+    var housingType: String
+    var numBedrooms: Int
+    var numBeds: Int
+    var rating: String
+    var numReviews: Int
+    var isSuperHost: Bool
+}
