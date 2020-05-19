4949830dd5427a5fe316505ee4778111fcb8b7bb
idevjinie <idevjinie@gmail.com>
2020-05-19 14:49:31 +0900

    [#1] feat :rocket: 커스텀 버튼 클래스 추가

---
+++ iOS/Airbnb/Airbnb/FilterButton.swift
@@ -0,0 +1,38 @@
+//
+//  FilterButton.swift
+//  Airbnb
+//
+//  Created by jinie on 2020/05/19.
+//  Copyright © 2020 jinie. All rights reserved.
+//
+
+import UIKit
+
+class FilterButton: UIButton {
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        configure()
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        
+        configure()
+    }
+    
+    private func configure() {
+        self.setTitleColor(.lightGray, for: .normal)
+        self.layer.borderWidth = 1.0
+        self.layer.borderColor = UIColor.lightGray.cgColor
+        self.layer.cornerRadius = self.bounds.height / 2
+        self.layer.masksToBounds = true
+        self.contentEdgeInsets = UIEdgeInsets(top: 8.0, left: 12.0, bottom: 8.0, right: 12.0)
+    }
+    
+    func configureTitle(_ text: String) {
+        self.setTitle(text, for: .normal)
+    }
+
+}
