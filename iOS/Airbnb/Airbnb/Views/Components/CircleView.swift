d7ca8ecbeb8de6ee544c461d730c936fa9855e25
delma <sdy2856@gmail.com>
2020-05-20 22:28:16 +0900

    [#3] feat :rocket: 동그란 뷰 그려주는 CircleView 생성

---
+++ iOS/Airbnb/Airbnb/Views/Components/CircleView.swift
@@ -0,0 +1,38 @@
+//
+//  CircleView.swift
+//  Airbnb
+//
+//  Created by delma on 2020/05/20.
+//  Copyright © 2020 jinie. All rights reserved.
+//
+
+import UIKit
+
+class CircleView: UIView {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configure()
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        configure()
+    }
+    
+    private func configure() {
+        self.layer.cornerRadius = self.bounds.size.height / 2
+        self.clipsToBounds = true
+        self.backgroundColor = .black
+        self.alpha = 0.0
+    }
+    
+    func show() {
+        self.alpha = 1.0
+    }
+    
+    func hide() {
+        self.alpha = 0.0
+    }
+}
+
