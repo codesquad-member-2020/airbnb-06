ba29a57f49e53df5a27b32a054ca5e9af5d6b29f
idevjinie <idevjinie@gmail.com>
2020-05-20 16:24:22 +0900

    [#3] feat :rocket: 캘린더 컬렉션뷰 생성

---
+++ iOS/Airbnb/Airbnb/ViewControllers/CalendarViewController.swift
@@ -0,0 +1,51 @@
+//
+//  CalendarViewController.swift
+//  Airbnb
+//
+//  Created by jinie on 2020/05/20.
+//  Copyright © 2020 jinie. All rights reserved.
+//
+
+import UIKit
+
+class CalendarViewController: UIViewController {
+
+    @IBOutlet weak var calendarCollectionView: UICollectionView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configure()
+    }
+    
+    private func configure() {
+        calendarCollectionView.dataSource = self
+        calendarCollectionView.delegate = self
+    }
+
+}
+
+extension CalendarViewController: UICollectionViewDataSource {
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 35
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "CalendarCell", for: indexPath)
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, viewForSupplementaryElementOfKind kind: String, at indexPath: IndexPath) -> UICollectionReusableView {
+        let headerView = collectionView.dequeueReusableSupplementaryView(ofKind: kind, withReuseIdentifier: "CalendarHeader", for: indexPath)
+        return headerView
+    }
+    
+}
+
+extension CalendarViewController: UICollectionViewDelegateFlowLayout {
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: self.view.bounds.width / 7, height: self.view.bounds.width / 7)
+    }
+    
+}
