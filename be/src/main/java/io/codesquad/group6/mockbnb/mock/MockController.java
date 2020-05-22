8c28626479c02c16fe40d78def48659e5a58bcaf
David Chung <david215@berkeley.edu>
2020-05-22 18:14:18 +0900

    feat :rocket: 숙소 상세 mock API 구현
    - ListingResponse를 ListingSummary와 ListingDetail로 구분.
    
    - Issue #19

--- be/src/main/java/io/codesquad/group6/mockbnb/mock/MockController.java
+++ be/src/main/java/io/codesquad/group6/mockbnb/mock/MockController.java
@@ -1,7 +1,8 @@
 package io.codesquad.group6.mockbnb.mock;
 
 import io.codesquad.group6.mockbnb.api.request.LikeRequest;
-import io.codesquad.group6.mockbnb.api.response.ListingResponse;
+import io.codesquad.group6.mockbnb.api.response.ListingDetail;
+import io.codesquad.group6.mockbnb.api.response.ListingSummary;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -17,11 +18,11 @@ import java.util.ArrayList;
 import java.util.List;
 
 @RestController
-@Slf4j()
+@Slf4j
 public class MockController {
 
     @GetMapping("/mock/listings")
-    public ResponseEntity<List<ListingResponse>> getListings(
+    public ResponseEntity<List<ListingSummary>> getListings(
             @RequestParam(required = false, defaultValue = "la") String city,
             @RequestParam(required = false, defaultValue = "2020-05-22") LocalDate checkin,
             @RequestParam(required = false, defaultValue = "2020-05-23") LocalDate checkout,
@@ -31,49 +32,49 @@ public class MockController {
             @RequestParam(required = false, defaultValue = "0") int offset,
             @RequestParam(required = false, defaultValue = "10") int limit,
             @RequestParam(name = "is-liked", required = false, defaultValue = "false") boolean isLiked) {
-        List<ListingResponse> listings = new ArrayList<>();
-        listings.add(ListingResponse.builder()
-                                    .id(1)
-                                    .name("listing1")
-                                    .imageUrl("https://a0.muscache.com/im/pictures/dd850460-46df-4422-a98b-86991f8de674.jpg?aki_policy=large")
-                                    .imageUrl("https://a0.muscache.com/im/pictures/38165109-1f28-429f-b9fd-99d0932c154e.jpg?aki_policy=large")
-                                    .imageUrl("https://a0.muscache.com/im/pictures/df7f56e6-71c0-4402-96de-27604ee4d460.jpg?aki_policy=large")
-                                    .housingType("Entire home/apt")
-                                    .numBedrooms(2)
-                                    .numBeds(3)
-                                    .rating(4.85)
-                                    .numReviews(210)
-                                    .isSuperHost(true)
-                                    .isLiked(true)
-                                    .build());
-        listings.add(ListingResponse.builder()
-                                    .id(2)
-                                    .name("listing2")
-                                    .imageUrl("https://a0.muscache.com/im/pictures/2b5214b8-2e0a-4fcb-8004-0f4b95bdcaa4.jpg?aki_policy=large")
-                                    .imageUrl("https://a0.muscache.com/im/pictures/100627966/2ccec29c_original.jpg?aki_policy=large")
-                                    .imageUrl("https://a0.muscache.com/im/pictures/92182afd-7553-42a5-8b41-98d60a600062.jpg?aki_policy=large")
-                                    .housingType("Private room")
-                                    .numBedrooms(1)
-                                    .numBeds(2)
-                                    .rating(4.93)
-                                    .numReviews(52)
-                                    .isSuperHost(false)
-                                    .isLiked(false)
-                                    .build());
-        listings.add(ListingResponse.builder()
-                                    .id(3)
-                                    .name("listing3")
-                                    .imageUrl("https://a0.muscache.com/im/pictures/1fa21b5c-b533-49f9-9569-a6808af8bc29.jpg?aki_policy=large")
-                                    .imageUrl("https://a0.muscache.com/im/pictures/108111917/046dd104_original.jpg?aki_policy=large")
-                                    .imageUrl("https://a0.muscache.com/im/pictures/b2142aa3-f9db-4c32-8eaa-3bf0c6b548c1.jpg?aki_policy=large")
-                                    .housingType("House")
-                                    .numBedrooms(3)
-                                    .numBeds(6)
-                                    .rating(4.77)
-                                    .numReviews(33)
-                                    .isSuperHost(false)
-                                    .isLiked(true)
-                                    .build());
+        List<ListingSummary> listings = new ArrayList<>();
+        listings.add(ListingSummary.builder()
+                                   .id(1)
+                                   .name("listing1")
+                                   .imageUrl("https://a0.muscache.com/im/pictures/dd850460-46df-4422-a98b-86991f8de674.jpg?aki_policy=large")
+                                   .imageUrl("https://a0.muscache.com/im/pictures/38165109-1f28-429f-b9fd-99d0932c154e.jpg?aki_policy=large")
+                                   .imageUrl("https://a0.muscache.com/im/pictures/df7f56e6-71c0-4402-96de-27604ee4d460.jpg?aki_policy=large")
+                                   .housingType("Entire home/apt")
+                                   .numBedrooms(2)
+                                   .numBeds(3)
+                                   .rating(4.85)
+                                   .numReviews(210)
+                                   .isSuperHost(true)
+                                   .isLiked(true)
+                                   .build());
+        listings.add(ListingSummary.builder()
+                                   .id(2)
+                                   .name("listing2")
+                                   .imageUrl("https://a0.muscache.com/im/pictures/2b5214b8-2e0a-4fcb-8004-0f4b95bdcaa4.jpg?aki_policy=large")
+                                   .imageUrl("https://a0.muscache.com/im/pictures/100627966/2ccec29c_original.jpg?aki_policy=large")
+                                   .imageUrl("https://a0.muscache.com/im/pictures/92182afd-7553-42a5-8b41-98d60a600062.jpg?aki_policy=large")
+                                   .housingType("Private room")
+                                   .numBedrooms(1)
+                                   .numBeds(2)
+                                   .rating(4.93)
+                                   .numReviews(52)
+                                   .isSuperHost(false)
+                                   .isLiked(false)
+                                   .build());
+        listings.add(ListingSummary.builder()
+                                   .id(3)
+                                   .name("listing3")
+                                   .imageUrl("https://a0.muscache.com/im/pictures/1fa21b5c-b533-49f9-9569-a6808af8bc29.jpg?aki_policy=large")
+                                   .imageUrl("https://a0.muscache.com/im/pictures/108111917/046dd104_original.jpg?aki_policy=large")
+                                   .imageUrl("https://a0.muscache.com/im/pictures/b2142aa3-f9db-4c32-8eaa-3bf0c6b548c1.jpg?aki_policy=large")
+                                   .housingType("House")
+                                   .numBedrooms(3)
+                                   .numBeds(6)
+                                   .rating(4.77)
+                                   .numReviews(33)
+                                   .isSuperHost(false)
+                                   .isLiked(true)
+                                   .build());
         return ResponseEntity.ok(listings);
     }
 
@@ -82,6 +83,28 @@ public class MockController {
                             @RequestBody LikeRequest likeRequest,
                             HttpServletResponse response) {
         response.setStatus(202);
+    }
+
+    @GetMapping("/mock/listings/{listing-id}")
+    public ResponseEntity<ListingDetail> getListingDetail(@PathVariable(name = "listing-id") long listingId) {
+        log.debug("listingId: {}", listingId);
+        return ResponseEntity.ok(ListingDetail.builder()
+                                              .imageUrl("https://a0.muscache.com/im/pictures/dd850460-46df-4422-a98b-86991f8de674.jpg?aki_policy=large")
+                                              .imageUrl("https://a0.muscache.com/im/pictures/38165109-1f28-429f-b9fd-99d0932c154e.jpg?aki_policy=large")
+                                              .imageUrl("https://a0.muscache.com/im/pictures/df7f56e6-71c0-4402-96de-27604ee4d460.jpg?aki_policy=large")
+                                              .isLiked(true)
+                                              .name("listing1")
+                                              .location("Korea Town, Los Angeles, California, United States")
+                                              .hostName("David")
+                                              .roomType("Entire home/apt")
+                                              .capacity(4)
+                                              .numBedrooms(2)
+                                              .numBeds(4)
+                                              .numBathrooms(2)
+                                              .price(123.45)
+                                              .rating(4.95)
+                                              .numReviews(123)
+                                              .build());
     }
 
 }
