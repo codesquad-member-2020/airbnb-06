62b38558d94ed293ca169c8b0544bc1d0d4ee813
delma <sdy2856@gmail.com>
2020-05-23 21:28:03 +0900

    [#13] feat :rocket: ASWebAuthenticationSession을 이용해 github 로그인 기능 구현

---
+++ iOS/Airbnb/Airbnb/ViewControllers/MainViewController.swift
@@ -0,0 +1,41 @@
+//
+//  MainViewController.swift
+//  Airbnb
+//
+//  Created by delma on 2020/05/23.
+//  Copyright © 2020 jinie. All rights reserved.
+//
+
+import UIKit
+import AuthenticationServices
+
+class MainViewController: UIViewController {
+
+    private var session: ASWebAuthenticationSession?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    @IBAction func githubLogin(_ sender: Any) {
+        authTokenWithWebLogin()
+    }
+    
+    private func authTokenWithWebLogin() {
+        let authURL = URL(string: "http://52.78.203.80/api/login")
+        let callbackURLScheme = "io.airbnb.app"
+        
+        self.session = ASWebAuthenticationSession(url: authURL!, callbackURLScheme: callbackURLScheme, completionHandler: { (callback: URL?, error: Error?) in
+            guard error == nil, let successURL = callback else { return }
+            let oAuthToken = URLComponents(string: successURL.absoluteString)?.queryItems?.filter({$0.name == "token"}).first?.value
+        })
+        self.session?.presentationContextProvider = self
+        self.session?.start()
+    }
+}
+
+extension MainViewController: ASWebAuthenticationPresentationContextProviding {
+    func presentationAnchor(for session: ASWebAuthenticationSession) -> ASPresentationAnchor {
+        return view.window!
+    }
+}
