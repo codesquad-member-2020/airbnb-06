c78812778a6ea836410a82c4fcddff6cd4462342
idevjinie <idevjinie@gmail.com>
2020-05-25 20:24:26 +0900

    [#5] feat :rocket: GuestViewController에 HeaderView 및 FooterView 추가

---
+++ iOS/Airbnb/Airbnb/ViewControllers/GuestViewController.swift
@@ -0,0 +1,17 @@
+//
+//  GuestViewController.swift
+//  Airbnb
+//
+//  Created by jinie on 2020/05/25.
+//  Copyright © 2020 jinie. All rights reserved.
+//
+
+import UIKit
+
+class GuestViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
---
+++ iOS/Airbnb/Airbnb/Views/GuestView/GuestSelectionView.swift
@@ -0,0 +1,21 @@
+//
+//  GuestSelectionView.swift
+//  Airbnb
+//
+//  Created by jinie on 2020/05/25.
+//  Copyright © 2020 jinie. All rights reserved.
+//
+
+import UIKit
+
+class GuestSelectionView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
