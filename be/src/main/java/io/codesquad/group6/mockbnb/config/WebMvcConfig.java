a1ebbfc35153a0fe61da5b34d3f226f8fd133b36
David Chung <david215@berkeley.edu>
2020-05-25 18:09:05 +0900

    feat :rocket: OAuth 라이브러리 추가
    - gradle 의존성 추가
    - WebMvcConfig ComponenentScan 추가
    - OAuth 컨트롤러 추가

--- be/src/main/java/io/codesquad/group6/mockbnb/config/WebMvcConfig.java
+++ be/src/main/java/io/codesquad/group6/mockbnb/config/WebMvcConfig.java
@@ -1,5 +1,6 @@
 package io.codesquad.group6.mockbnb.config;
 
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.format.FormatterRegistry;
 import org.springframework.format.datetime.standard.DateTimeFormatterRegistrar;
@@ -8,6 +9,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import java.time.format.DateTimeFormatter;
 
 @Configuration
+@ComponentScan("io.dc215.githuboauth")
 public class WebMvcConfig implements WebMvcConfigurer {
 
     @Override
