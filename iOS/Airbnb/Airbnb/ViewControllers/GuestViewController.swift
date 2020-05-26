fbd0bedabe7a25e4c4ba635236ca8f39e0bcf1ab
idevjinie <idevjinie@gmail.com>
2020-05-26 13:13:59 +0900

    [#5] feat :rocket: 인원 선택 화면의 스타일 조정

--- iOS/Airbnb/Airbnb/ViewControllers/GuestViewController.swift
+++ iOS/Airbnb/Airbnb/ViewControllers/GuestViewController.swift
@@ -10,8 +10,29 @@ import UIKit
 
 class GuestViewController: UIViewController {
 
+    @IBOutlet weak var contentView: UIView!
+    @IBOutlet weak var headerView: HeaderView!
+    @IBOutlet weak var adultSelectionView: GuestSelectionView!
+    @IBOutlet weak var childSelectionView: GuestSelectionView!
+    @IBOutlet weak var infantSelectionView: GuestSelectionView!
+    @IBOutlet weak var footerView: FooterView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        configure()
+    }
+    
+    private func configure() {
+        contentView.layer.cornerRadius = 12.0
+        contentView.layer.masksToBounds = true
+        headerView.titleLabel.text = "인원"
+        adultSelectionView.ageGroupLabel.text = "성인"
+        adultSelectionView.detailLabel.text = "만13세 이상"
+        childSelectionView.ageGroupLabel.text = "청소년"
+        childSelectionView.detailLabel.text = "만3세 ~ 만12세"
+        infantSelectionView.ageGroupLabel.text = "영유아"
+        infantSelectionView.detailLabel.text = "만2세 이하"
     }
 
 }
--- iOS/Airbnb/Airbnb/ViewControllers/SearchViewController.swift
+++ iOS/Airbnb/Airbnb/ViewControllers/SearchViewController.swift
@@ -19,10 +19,12 @@ class SearchViewController: UIViewController {
     @IBOutlet weak var floatingButton: Floaty!
     
     private var accommodationSearchDataSource: AccommodationSearchCollectionViewDataSource!
-    private let identifiers: [String : String] = ["날짜": "CalendarViewController", "인원": "GuestViewController"]
+    private let identifiers: [String : String] = ["날짜": "CalendarViewController",
+                                                  "인원": "GuestViewController",
+                                                  "가격": "PriceViewController"]
     
     @IBAction func displayPopup(_ sender: UIButton) {
-        guard let title = sender.titleLabel?.text else { return }
+        guard let title = sender.currentTitle else { return }
         guard let viewController = storyboard?.instantiateViewController(withIdentifier: identifiers[title]!) else { return }
         viewController.modalPresentationStyle = .overCurrentContext
         viewController.modalTransitionStyle = .crossDissolve
