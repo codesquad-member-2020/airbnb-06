576e660fb178674af07ac7d2ead391fdefb9d2e1
delma <sdy2856@gmail.com>
2020-05-27 16:27:22 +0900

    [#40] feat :rocket: EndPoints 생성

---
+++ iOS/Airbnb/Airbnb/Network/EndPoints.swift
@@ -0,0 +1,16 @@
+//
+//  EndPoints.swift
+//  Airbnb
+//
+//  Created by delma on 2020/05/27.
+//  Copyright © 2020 jinie. All rights reserved.
+//
+
+import Foundation
+
+enum EndPoints {
+    static let defaultURL = "http://52.78.203.80/api/"
+    static let login = "login"
+    static let listings = "listings"
+    static let bookings = "bookings"
+}
---
+++ iOS/Airbnb/Airbnb/Network/NetworkManager.swift
@@ -0,0 +1,9 @@
+//
+//  NetworkManager.swift
+//  Airbnb
+//
+//  Created by delma on 2020/05/27.
+//  Copyright © 2020 jinie. All rights reserved.
+//
+
+import Foundation
