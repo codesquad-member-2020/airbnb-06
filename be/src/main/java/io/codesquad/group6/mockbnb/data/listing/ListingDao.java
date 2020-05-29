9ab079b9b2d21869a8aea7a85d8a6a92f5ac046f
David Chung <david215@berkeley.edu>
2020-05-29 22:09:50 +0900

    [#44] :sparkles: 숙소 목록 쿼리 작성

--- be/src/main/java/io/codesquad/group6/mockbnb/data/listing/ListingDao.java
+++ be/src/main/java/io/codesquad/group6/mockbnb/data/listing/ListingDao.java
@@ -20,7 +20,32 @@ public class ListingDao {
     }
 
     public List<Listing> findListings(ListingFilter listingFilter) {
-        return null;
+        String sql = "SELECT l.id, l.name, l.housing_type, l.capacity, l.num_bathrooms, l.num_bedrooms, l.num_beds, " +
+                         "l.price, l.cleaning_fee, l.num_reviews, l.rating, l.latitude, l.longitude, " +
+                         "CONCAT_WS(', ', l.neighborhood, l.city, l.state, l.country) AS l_location, " +
+                         "EXISTS(SELECT bm.id " +
+                             "FROM bookmark bm " +
+                             "WHERE bm.guest = :g_id " +
+                                 "AND bm.listing = l.id) AS l_is_bookmarked, " +
+                         "(SELECT GROUP_CONCAT(i.image_url) " +
+                             "FROM image i " +
+                             "WHERE i.listing = l.id) AS l_image_urls, " +
+                         "h.id, h.name, h.is_superhost " +
+                     "FROM listing l " +
+                     "JOIN host h ON l.host = h.id " +
+                         "WHERE l.capacity >= :num_guests " +
+                             "AND l.price BETWEEN :min_price AND :max_price " +
+                             "AND l.latitude BETWEEN :min_latitude AND :max_latitude " +
+                             "AND l.longitude BETWEEN :min_longitude AND :max_longitude " +
+                             "AND NOT EXISTS(SELECT b.id " +
+                                 "FROM booking b " +
+                                 "WHERE b.listing = l.id " +
+                                 "AND ((b.checkin BETWEEN :checkin AND :checkout) " +
+                                 "OR (b.checkout BETWEEN :checkin AND :checkout) " +
+                                 "OR (b.checkin < :checkin AND :checkout < checkout))) " +
+                     "LIMIT :limit OFFSET :offset";
+        SqlParameterSource sqlParameterSource = listingFilter.toSqlParameterSource();
+        return namedParameterJdbcTemplate.query(sql, sqlParameterSource, ListingMapper.instance());
     }
 
     public Listing findListingById(long listingId, long guestId) {
