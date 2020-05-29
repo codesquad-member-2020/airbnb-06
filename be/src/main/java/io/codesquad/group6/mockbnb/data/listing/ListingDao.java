90292160b9c31d99f8a018d5c8d582b5d3f65219
David Chung <david215@berkeley.edu>
2020-05-29 21:11:18 +0900

    [#44] :zap: 쿼리 최적화
    - SELECT 서브쿼리로 로직으로 JOIN을 대체.
    - 필요없는 파라미터 제거.

--- be/src/main/java/io/codesquad/group6/mockbnb/data/listing/ListingDao.java
+++ be/src/main/java/io/codesquad/group6/mockbnb/data/listing/ListingDao.java
@@ -24,18 +24,19 @@ public class ListingDao {
     }
 
     public Listing findListingById(long listingId, long guestId) {
-        String sql = "SELECT l.id, l.name, CONCAT_WS(', ', l.neighborhood, l.city, l.state, l.country) AS l_location, " +
-                         "l.latitude, l.longitude, l.housing_type, l.capacity, l.num_bathrooms, l.num_bedrooms, l.num_beds, " +
-                         "l.price, l.cleaning_fee, l.num_reviews, l.rating, " +
+        String sql = "SELECT l.id, l.name, l.housing_type, l.capacity, l.num_bathrooms, l.num_bedrooms, l.num_beds, " +
+                         "l.price, l.cleaning_fee, l.num_reviews, l.rating, l.latitude, l.longitude, " +
+                         "CONCAT_WS(', ', l.neighborhood, l.city, l.state, l.country) AS l_location, " +
+                         "(SELECT GROUP_CONCAT(i.image_url) " +
+                         "FROM image i " +
+                         "WHERE i.listing = l.id) AS l_image_urls, " +
                          "EXISTS(SELECT b.id " +
                              "FROM bookmark b " +
                              "WHERE b.guest = :g_id " +
                                  "AND b.listing = l.id) AS l_is_bookmarked, " +
-                         "GROUP_CONCAT(i.image_url) AS l_image_urls, " +
                          "h.id, h.name, h.is_superhost " +
                      "FROM listing l " +
                          "JOIN host h ON l.host = h.id " +
-                         "JOIN image i ON l.id = i.listing " +
                      "WHERE l.id = :l_id";
         SqlParameterSource sqlParameterSource = new MapSqlParameterSource().addValue("l_id", listingId)
                                                                            .addValue("g_id", guestId);
