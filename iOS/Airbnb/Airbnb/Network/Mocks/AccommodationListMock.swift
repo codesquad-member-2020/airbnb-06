bd26414e14a7802b56ee4ffab7004adfef54144d
delma <sdy2856@gmail.com>
2020-05-30 16:29:44 +0900

    [#53] refactor :recycle: 들여쓰기 덜하기 위해 if else를 guard else로 변경

--- iOS/Airbnb/Airbnb/Network/Mocks/AccommodationListMock.swift
+++ iOS/Airbnb/Airbnb/Network/Mocks/AccommodationListMock.swift
@@ -10,14 +10,13 @@ import Foundation
 
 struct AccommodationListMock {
     func request(handler: @escaping ([Accommodation]) -> Void) {
-        if let path = Bundle.main.path(forResource: "FakeAccommodationList", ofType: "json") {
-            do {
-                let data = try Data(contentsOf: URL(fileURLWithPath: path), options: .mappedIfSafe)
-                let decodedData = try JSONDecoder().decode([Accommodation].self, from: data)
-                handler(decodedData)
-            } catch {
-                print(error.localizedDescription)
-            }
+        guard let path = Bundle.main.path(forResource: "FakeAccommodationList", ofType: "json") else { return }
+        do {
+            let data = try Data(contentsOf: URL(fileURLWithPath: path), options: .mappedIfSafe)
+            let decodedData = try JSONDecoder().decode([Accommodation].self, from: data)
+            handler(decodedData)
+        } catch {
+            print(error.localizedDescription)
         }
     }
 }
