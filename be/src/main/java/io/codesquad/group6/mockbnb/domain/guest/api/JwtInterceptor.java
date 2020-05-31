7b8e349a15acac1abd01e9543f295453e0423e16
David Chung <david215@berkeley.edu>
2020-06-01 03:10:58 +0900

    [#45] :sparkles: 인터셉터 로직 추가
    - 즐겨찾기 목록 같이 사적인 개인 데이터를 요청하는 경우에는 유효한 토큰을
      포함하지 않으면 401 Unauthorized 에러로 응답하도록 로직 추가.

--- be/src/main/java/io/codesquad/group6/mockbnb/domain/guest/api/JwtInterceptor.java
+++ be/src/main/java/io/codesquad/group6/mockbnb/domain/guest/api/JwtInterceptor.java
@@ -1,14 +1,17 @@
 package io.codesquad.group6.mockbnb.domain.guest.api;
 
+import io.codesquad.group6.mockbnb.domain.guest.domain.GuestService;
 import io.codesquad.group6.mockbnb.githuboauth.GitHubUserData;
 import io.codesquad.group6.mockbnb.githuboauth.JwtService;
-import io.codesquad.group6.mockbnb.domain.guest.domain.GuestService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.web.servlet.HandlerInterceptor;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.HashSet;
+import java.util.Set;
 
 @Service
 @Slf4j
@@ -16,22 +19,43 @@ public class JwtInterceptor implements HandlerInterceptor {
 
     private final JwtService jwtService;
     private final GuestService guestService;
+    private final Set<String> privateEndpoints;
 
     public JwtInterceptor(JwtService jwtService, GuestService guestService) {
         this.jwtService = jwtService;
         this.guestService = guestService;
+        privateEndpoints = new HashSet<>();
+        privateEndpoints.add("/listings/bookmarks");
     }
 
     @Override
-    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) {
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws IOException {
+        String jwt = getJwtFromRequestHeader(request);
+        long guestId = getGuestIdFromJwt(jwt);
+        request.setAttribute("guestId", guestId);
+        return okToProceed(request, response, guestId);
+    }
+
+    private String getJwtFromRequestHeader(HttpServletRequest request) {
         String jwt = request.getHeader("Authorization");
         log.debug("jwt: {}", jwt);
-        if (jwt != null) {
-            GitHubUserData gitHubUserData = jwtService.parseJwt(jwt);
-            guestService.validateGuestData(gitHubUserData);
-            request.setAttribute("guestId", gitHubUserData.getId());
-        } else {
-            request.setAttribute("guestId", -1L); // will be handled in DAO
+        return jwt;
+    }
+
+    private long getGuestIdFromJwt(String jwt) {
+        if (jwt == null) {
+            return -1L;
+        }
+        GitHubUserData gitHubUserData = jwtService.parseJwt(jwt); // may throw JwtException
+        guestService.validateGuestData(gitHubUserData); // may throw InvalidGuestDataException
+        return gitHubUserData.getId();
+    }
+
+    private boolean okToProceed(HttpServletRequest request, HttpServletResponse response, long guestId) throws IOException {
+        String requestEndpoint = request.getServletPath();
+        if (privateEndpoints.contains(requestEndpoint) && guestId == -1L) {
+            response.sendError(401, "Requested operation requires authentication.");
+            return false;
         }
         return true;
     }
