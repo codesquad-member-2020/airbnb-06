70895cd64fda3930b97a1ac810b3f23e2826b102
idevjinie <idevjinie@gmail.com>
2020-06-02 03:04:37 +0900

    [#7] feat :rocket: 가격 범위를 조정하는 PriceRangeSlider 클래스 생성

--- iOS/Airbnb/Airbnb/ViewControllers/PriceRangeViewController.swift
+++ iOS/Airbnb/Airbnb/ViewControllers/PriceRangeViewController.swift
@@ -14,17 +14,29 @@ class PriceRangeViewController: UIViewController {
     @IBOutlet weak var headerView: PopupHeaderView!
     @IBOutlet weak var averageLabel: UILabel!
     @IBOutlet weak var graphView: PriceRangeGraphView!
+    @IBOutlet weak var sliderView: UIView!
     @IBOutlet weak var footerView: PopupFooterView!
+    
+    private let slider = PriceRangeSlider(frame: .zero)
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
         configureView()
+        configureSlider()
+    }
+    
+    override func viewDidLayoutSubviews() {
+        slider.frame = CGRect(x: 0, y: 0, width: sliderView.bounds.width, height: sliderView.bounds.height)
     }
     
     private func configureView() {
         contentView.layer.cornerRadius = 12.0
         contentView.layer.masksToBounds = true
         headerView.titleLabel.text = "가격"
+    }
+    
+    private func configureSlider() {
+        sliderView.addSubview(slider)
     }
 }
