f0bfab1377a969d2cd4a7c0e6bd7a235407e3b84
idevjinie <idevjinie@gmail.com>
2020-06-01 21:09:40 +0900

    [#7] feat :rocket: 가격 관련 label 구성

--- iOS/Airbnb/Airbnb/ViewControllers/PriceRangeViewController.swift
+++ iOS/Airbnb/Airbnb/ViewControllers/PriceRangeViewController.swift
@@ -9,8 +9,21 @@
 import UIKit
 
 class PriceRangeViewController: UIViewController {
+    
+    @IBOutlet weak var contentView: UIView!
+    @IBOutlet weak var headerView: PopupHeaderView!
+    @IBOutlet weak var averageLabel: UILabel!
+    @IBOutlet weak var footerView: PopupFooterView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        configureView()
+    }
+    
+    private func configureView() {
+        contentView.layer.cornerRadius = 12.0
+        contentView.layer.masksToBounds = true
+        headerView.titleLabel.text = "가격"
     }
 }
