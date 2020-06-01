a6b16c37cc8711ad151ecf8f8d4a4abf3bae1662
idevjinie <idevjinie@gmail.com>
2020-06-01 20:39:09 +0900

    [#7] feat :rocket: PriceRangeViewController 클래스 생성 및 지정"

---
+++ iOS/Airbnb/Airbnb/ViewControllers/PriceRangeViewController.swift
@@ -0,0 +1,16 @@
+//
+//  PriceRangeViewController.swift
+//  Airbnb
+//
+//  Created by jinie on 2020/06/01.
+//  Copyright © 2020 jinie. All rights reserved.
+//
+
+import UIKit
+
+class PriceRangeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
