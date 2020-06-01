d9c13c2fb4228ca894666f324ef3844748c545d0
David Chung <david215@berkeley.edu>
2020-06-02 00:43:26 +0900

    [#47] :recycle: 객체 메서드로 리팩토링
    - 더 객체지향적인 방향으로 리팩토링.

--- be/src/main/java/io/codesquad/group6/mockbnb/domain/listing/domain/Listing.java
+++ be/src/main/java/io/codesquad/group6/mockbnb/domain/listing/domain/Listing.java
@@ -1,5 +1,7 @@
 package io.codesquad.group6.mockbnb.domain.listing.domain;
 
+import io.codesquad.group6.mockbnb.domain.listing.api.dto.response.ListingDetail;
+import io.codesquad.group6.mockbnb.domain.listing.api.dto.response.ListingSummary;
 import lombok.Builder;
 import lombok.Value;
 import org.springframework.data.annotation.Id;
@@ -29,5 +31,40 @@ public class Listing {
     double longitude;
     boolean isSuperHost;
     boolean isBookmarked;
+
+    public ListingSummary toListingSummary() {
+        return ListingSummary.builder()
+                             .id(id)
+                             .name(name)
+                             .imageUrls(imageUrls)
+                             .housingType(housingType)
+                             .numBedrooms(numBedrooms)
+                             .numBeds(numBeds)
+                             .rating(rating)
+                             .numReviews(numReviews)
+                             .isSuperHost(isSuperHost)
+                             .isBookmarked(isBookmarked)
+                             .latitude(latitude)
+                             .longitude(longitude)
+                             .build();
+    }
+
+    public ListingDetail toListingDetail() {
+        return ListingDetail.builder()
+                            .imageUrls(imageUrls)
+                            .isBookmarked(isBookmarked)
+                            .name(name)
+                            .location(location)
+                            .hostName(hostName)
+                            .roomType(roomType)
+                            .capacity(capacity)
+                            .numBedrooms(numBedrooms)
+                            .numBeds(numBeds)
+                            .numBathrooms(numBathrooms)
+                            .price(price)
+                            .rating(rating)
+                            .numReviews(numReviews)
+                            .build();
+    }
 
 }
--- be/src/main/java/io/codesquad/group6/mockbnb/domain/listing/domain/ListingService.java
+++ be/src/main/java/io/codesquad/group6/mockbnb/domain/listing/domain/ListingService.java
@@ -26,13 +26,13 @@ public class ListingService {
     public List<ListingSummary> getListings(ListingFilter listingFilter) {
         List<Listing> listings = listingDao.findListings(listingFilter);
         return listings.stream()
-                       .map(this::mapToListingSummary)
+                       .map(Listing::toListingSummary)
                        .collect(Collectors.toList());
     }
 
     public ListingDetail getListing(long listingId, long guestId) {
         Listing listing = listingDao.findListingById(listingId, guestId);
-        return mapToListingDetail(listing);
+        return listing.toListingDetail();
     }
 
     public PriceGraphData getPriceGraphData(LocalDate checkin, LocalDate checkout, int numGuests) {
@@ -50,43 +50,8 @@ public class ListingService {
     public List<ListingSummary> getBookmarkedListings(long guestId) {
         List<Listing> bookmarkedListings = listingDao.findBookmarkedListings(guestId);
         return bookmarkedListings.stream()
-                                 .map(this::mapToListingSummary)
+                                 .map(Listing::toListingSummary)
                                  .collect(Collectors.toList());
-    }
-
-    private ListingSummary mapToListingSummary(Listing listing) {
-        return ListingSummary.builder()
-                             .id(listing.getId())
-                             .name(listing.getName())
-                             .imageUrls(listing.getImageUrls())
-                             .housingType(listing.getHousingType())
-                             .numBedrooms(listing.getNumBedrooms())
-                             .numBeds(listing.getNumBeds())
-                             .rating(listing.getRating())
-                             .numReviews(listing.getNumReviews())
-                             .isSuperHost(listing.isSuperHost())
-                             .isBookmarked(listing.isBookmarked())
-                             .latitude(listing.getLatitude())
-                             .longitude(listing.getLongitude())
-                             .build();
-    }
-
-    private ListingDetail mapToListingDetail(Listing listing) {
-        return ListingDetail.builder()
-                            .imageUrls(listing.getImageUrls())
-                            .isBookmarked(listing.isBookmarked())
-                            .name(listing.getName())
-                            .location(listing.getLocation())
-                            .hostName(listing.getHostName())
-                            .roomType(listing.getRoomType())
-                            .capacity(listing.getCapacity())
-                            .numBedrooms(listing.getNumBedrooms())
-                            .numBeds(listing.getNumBeds())
-                            .numBathrooms(listing.getNumBathrooms())
-                            .price(listing.getPrice())
-                            .rating(listing.getRating())
-                            .numReviews(listing.getNumReviews())
-                            .build();
     }
 
 }
