88450d52ca806ddc7c75db841e4cbb6be4011908
delma <sdy2856@gmail.com>
2020-06-04 16:23:12 +0900

    [#62] feat :rocket: networkDispatcher의 implement 메소드 실행 및 statusCode에 따라 분기처리, request의 타입을 URLRequest로 변경

--- iOS/Airbnb/Airbnb/Network/UseCases/AccommodationUseCase.swift
+++ iOS/Airbnb/Airbnb/Network/UseCases/AccommodationUseCase.swift
@@ -9,10 +9,10 @@
 import Foundation
 
 struct AccommodationUseCase {
-    var request: Request
+    var request: URLRequest
     var networkDispatcher: NetworkDispatcher
     
-    init(request: Request, networkDispatcher: NetworkDispatcher) {
+    init(request: URLRequest, networkDispatcher: NetworkDispatcher) {
         self.request = request
         self.networkDispatcher = networkDispatcher
     }
@@ -22,6 +22,17 @@ struct AccommodationUseCase {
             guard let data = anyData else { return }
             guard let decodeData = try? JSONDecoder().decode(dataType, from: data) else { return }
             handler(decodeData)
+        }
+    }
+    
+    func perform(id: Int, handler: @escaping (Result<Any, NetworkError>) -> Void) {
+        networkDispatcher.implement(request: request) { statusCode in
+            switch statusCode {
+            case 200...202:
+                return handler(.success(true))
+            default:
+                return handler(.failure(.BadRequest))
+            }
         }
     }
     
