73f0bf3aad34f3c4ea7b8a64c7c36d40fc88c268
delma <sdy2856@gmail.com>
2020-06-04 19:51:51 +0900

    [#62] feat :rocket: 필터링할 모델 값을 관리할 뷰모델 생성

--- iOS/Airbnb/Airbnb/ViewModels/SearchViewModel.swift
+++ iOS/Airbnb/Airbnb/ViewModels/SearchViewModel.swift
@@ -7,3 +7,38 @@
 //
 
 import Foundation
+
+class SearchViewModel: ViewModelBinding {
+    typealias Key = FilteringCondition
+    typealias handler = (FilteringCondition) -> Void
+    private var changedHandler: handler
+    private var filteringCondition: Key {
+        didSet {
+            changedHandler(filteringCondition)
+        }
+    }
+    
+    init(filteringCondition: FilteringCondition, handler: @escaping handler) {
+        self.filteringCondition = filteringCondition
+        self.changedHandler = handler
+        changedHandler(filteringCondition)
+    }
+    
+    func updateNotify(changed handler: @escaping (FilteringCondition) -> Void) {
+        self.changedHandler = handler
+    }
+    
+    func update(checkIn: String, checkOut: String) {
+        filteringCondition.checkIn = checkIn
+        filteringCondition.checkOut = checkOut
+    }
+    
+    func update(minPrice: String, maxPrice: String) {
+        filteringCondition.minPrice = minPrice
+        filteringCondition.maxPrice = maxPrice
+    }
+    
+    func update(guestCount: String) {
+        filteringCondition.guestCount = guestCount
+    }
+}
