d828fa66a6bb0ebbba69cd3bd4152851efa0d574
delma <sdy2856@gmail.com>
2020-06-04 19:50:55 +0900

    [#62] feat :rocket: filtering할 값들 담을 모델 생성

---
+++ iOS/Airbnb/Airbnb/Models/FilteringCondition.swift
@@ -0,0 +1,17 @@
+//
+//  FilteringCondition.swift
+//  Airbnb
+//
+//  Created by delma on 2020/06/04.
+//  Copyright © 2020 jinie. All rights reserved.
+//
+
+import Foundation
+
+struct FilteringCondition {
+    var checkIn: String?
+    var checkOut: String?
+    var guestCount: String?
+    var minPrice: String?
+    var maxPrice: String?
+}
---
+++ iOS/Airbnb/Airbnb/ViewModels/SearchViewModel.swift
@@ -0,0 +1,9 @@
+//
+//  SearchViewModel.swift
+//  Airbnb
+//
+//  Created by delma on 2020/06/04.
+//  Copyright © 2020 jinie. All rights reserved.
+//
+
+import Foundation
