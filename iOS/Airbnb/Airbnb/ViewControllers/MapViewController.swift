8105d6914a201fe7c98d7d06b9a6bd1945a65edf
idevjinie <idevjinie@gmail.com>
2020-06-05 12:02:37 +0900

    [#11] MapViewController 클래스 생성

---
+++ iOS/Airbnb/Airbnb/ViewControllers/MapViewController.swift
@@ -0,0 +1,20 @@
+//
+//  MapViewController.swift
+//  Airbnb
+//
+//  Created by jinie on 2020/06/05.
+//  Copyright © 2020 jinie. All rights reserved.
+//
+
+import UIKit
+import MapKit
+import Alamofire
+
+class MapViewController: UIViewController {
+    
+    @IBOutlet weak var mapView: MKMapView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
