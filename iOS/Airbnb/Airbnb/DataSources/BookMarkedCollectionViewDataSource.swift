0d13eb39ccbcf20257ac86b1bef2c25876073b96
delma <sdy2856@gmail.com>
2020-06-05 11:16:52 +0900

    [#67] feat :rocket: BookMarkedCollectionViewDataSource 생성

--- iOS/Airbnb/Airbnb/DataSources/BookMarkedCollectionViewDataSource.swift
+++ iOS/Airbnb/Airbnb/DataSources/BookMarkedCollectionViewDataSource.swift
@@ -6,4 +6,23 @@
 //  Copyright © 2020 jinie. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+class BookMarkedCollectionViewDataSource: NSObject, UICollectionViewDataSource {
+    
+    private var accommodations: [Accommodation]
+    
+    init(accommodations: [Accommodation]) {
+        self.accommodations = accommodations
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return accommodations.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "AccommodationSearchCollectionViewCell", for: indexPath) as! AccommodationSearchCollectionViewCell
+        cell.configureData(accommodations[indexPath.row])
+        return cell
+    }
+}
